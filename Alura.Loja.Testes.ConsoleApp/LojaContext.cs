7b160eb0852ddb1c75ee9666e101a6001be6e7ab
haroldo <haroldo.gmail.com>
2020-04-14 22:39:30 -0300

    Add project files.

---
+++ Alura.Loja.Testes.ConsoleApp/LojaContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Alura.Loja.Testes.ConsoleApp
+{
+    public class LojaContext : DbContext
+    {
+        public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=LocalDB;Integrated Security=True;Pooling=False");
+        }
+    }
+}
